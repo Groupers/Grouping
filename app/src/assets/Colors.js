60c448939bcf8fcae8f56315ffb91ab98d008cc1
jinsumin <suminjin@naver.com>
2020-06-20 00:05:31 +0900

    add const colors

---
+++ app/src/assets/Colors.js
@@ -0,0 +1,10 @@
+export const COLORS = {
+    PRIMARY: '#f9f9f9',
+    WHITE: '#FFF',
+    WINE_RED: '#d70000',
+    GRAY: '#9c9c9c',
+    LIGHTER: '#F3F3F3',
+    LIGHT: '#DAE1E7',
+    DARK: '#444',
+    BLACK: '#000',
+};
