fc7269630ec16c6aa77e0417b935cab59499c322
jeonsanghyun <jeonsang@linecorp.com>
2020-06-20 21:19:31 +0900

    [NO-BTS] Add grouping creation view(description)

--- app/src/store/GroupingCreationMainStore.js
+++ app/src/store/GroupingCreationMainStore.js
@@ -3,12 +3,16 @@ import { GROUPING_CREATION_VIEW_STATUS } from '../constant/GroupingCreationViewS
 import GroupingCreationDto from '../dto/GroupingCreationDto';
 import KeywordParser from '../component/KeywordParser';
 
+const MIN_DESCRIPTION_LENGTH = 10;
+const MIN_TITLE_LENGTH = 2;
+
 export default class GroupingCreationMainStore {
   @observable groupingCreationViewStatus =
     GROUPING_CREATION_VIEW_STATUS.MAIN_INFO;
   @observable groupingCreationDto = new GroupingCreationDto();
   @observable groupingTitle = '';
   @observable groupingKeyword = '';
+  @observable groupingDescription = '';
 
   keywordParser = new KeywordParser();
 
@@ -20,13 +24,26 @@ export default class GroupingCreationMainStore {
     this.groupingKeyword = keyword;
   };
 
+  @action groupingDescriptionChanged = description => {
+    this.groupingDescription = description;
+  };
+
   @action isHeaderRightIconActivated = groupingCreationView => {
     if (
       this.groupingCreationViewStatus === groupingCreationView &&
       this.groupingCreationViewStatus ===
         GROUPING_CREATION_VIEW_STATUS.MAIN_INFO &&
-      this.groupingTitle.length > 2 &&
+      this.groupingTitle.length > MIN_TITLE_LENGTH &&
       this.keywordParser.parseKeyword(this.groupingKeyword)
+    ) {
+      return true;
+    }
+
+    if (
+      this.groupingCreationViewStatus === groupingCreationView &&
+      this.groupingCreationViewStatus ===
+        GROUPING_CREATION_VIEW_STATUS.DESCRIPTION &&
+      this.groupingDescription.length > MIN_DESCRIPTION_LENGTH
     ) {
       return true;
     }
