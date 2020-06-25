7aff5b498d4fc7af4af34b6e5404c4afde6551ec
JungEunPyo <sharron315@naver.com>
2020-06-25 14:51:09 +0900

    get FriendListdata by URL

--- app/src/repository/UserRepository.js
+++ app/src/repository/UserRepository.js
@@ -4,7 +4,7 @@ import auth from '@react-native-firebase/auth';
 
 const TARGET_URL = SERVER_URL + '/users';
 
-export default class UserRepository {
+export default class UserRepository{
   initialize = async () => {
     axios
       .post(TARGET_URL + '/auth', {})
