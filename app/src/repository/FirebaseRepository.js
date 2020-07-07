41522a644419844bd271212259752e6ccb682f1a
jinsumin <suminjin@naver.com>
2020-07-07 11:23:56 +0900

    set yarn eslint, prettier and change firebase phone auth verify -> signInWithPhoneNumber

--- app/src/repository/FirebaseRepository.js
+++ app/src/repository/FirebaseRepository.js
@@ -5,30 +5,8 @@ import { SERVER_URL } from '../constant/HttpProperty';
 const TARGET_URL = `${SERVER_URL}/users`;
 
 export default class FirebaseRepository {
-  /*
   sendSignUpPhoneCode = async (phoneNumber) => {
     const confirmation = await auth().signInWithPhoneNumber(phoneNumber);
-    return confirmation;
-  }
-   */
-  sendSignUpPhoneCode = async (phoneNumber, timeOut) => {
-    const confirmation = await auth()
-      .verifyPhoneNumber(phoneNumber, timeOut)
-      .on('state_changed', (phoneAuthSnapshot, timeOut) => {
-        if (phoneAuthSnapshot.state === auth.PhoneAuthState.CODE_SENT) {
-          return Promise.resolve();
-        }
-        return Promise.reject(new Error('Code not sent!'));
-      })
-      .then(
-        (phoneAuthSnapshot) => {
-          console.log(phoneAuthSnapshot.state);
-        },
-        (error) => {
-          console.error(error.message);
-        }
-      );
-    console.log(confirmation);
     return confirmation;
   };
 }
--- app/src/store/SignUpPhoneStore.js
+++ app/src/store/SignUpPhoneStore.js
@@ -81,10 +81,7 @@ export default class SignUpPhoneStore {
       return;
     }
     // 아래 코드 다시 예전 코드로 변경
-    this.codeConfirmation = await this.firebaseRepository.sendSignUpPhoneCode(
-      this.phoneNumber,
-      180
-    );
+    this.codeConfirmation = await this.firebaseRepository.sendSignUpPhoneCode(this.phoneNumber);
     console.log(this.codeConfirmation);
     this.phoneValidationViewStatus = SIGN_UP_PHONE_VIEW_STATUS.PHONE_NUMBER_SENT_AFTER;
   };
