63f6996835bac787cfb50d830add9788aa9f7157
jinsumin <suminjin@naver.com>
2020-08-06 18:04:24 +0900

    use touchable opacity instead button '+group creation'

--- app/src/view/main/home/HomeMain.js
+++ app/src/view/main/home/HomeMain.js
@@ -1,13 +1,15 @@
 import * as React from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, Text, TouchableOpacity, View } from 'react-native';
+import { Icon } from 'react-native-elements';
 
 // eslint-disable-next-line react/prop-types
 export default function HomeMain({ navigation }) {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <TouchableOpacity onPress={() => navigation.navigate('GroupCreation')}>
+        <Text>+그룹</Text>
+      </TouchableOpacity>
       <Text>Home screen</Text>
-      {/* eslint-disable-next-line react/prop-types */}
-      <Button title="create a new group" onPress={() => navigation.navigate('GroupCreation')} />
     </View>
   );
 }
