f720860034c0d38f9412983f76d1f9b225f81fd2
jinsumin <suminjin@naver.com>
2020-08-07 14:52:59 +0900

    modify naming

--- app/src/constant/FontSize.js
+++ app/src/constant/FontSize.js
@@ -1,10 +1,10 @@
-import { Dimensions } from 'react-native';
+// eslint-disable-next-line import/named
+import { WINDOW_SIZE } from './WindowSize';
 
-const Width = Dimensions.get('window').width;
-const Height = Dimensions.get('window').height;
-export const FontSize = {
-  LOGO: Height * 0.054, //40
-  TITLE: Height * 0.027, // 20
-  SUB_TITLE: Height * 0.0163, //12
-  LABEL: Height * 0.019, //14
+// eslint-disable-next-line import/prefer-default-export
+export const FONT_SIZE = {
+  LOGO: WINDOW_SIZE.HEIGHT * 0.054, // 40
+  TITLE: WINDOW_SIZE.HEIGHT * 0.027, // 20
+  SUB_TITLE: WINDOW_SIZE.HEIGHT * 0.0163, // 12
+  LABEL: WINDOW_SIZE.HEIGHT * 0.019, // 14
 };
---
+++ app/src/constant/WindowSize.js
@@ -0,0 +1,7 @@
+import { Dimensions } from 'react-native';
+
+// eslint-disable-next-line import/prefer-default-export
+export const WINDOW_SIZE = {
+  HEIGHT: Dimensions.get('window').height,
+  WIDTH: Dimensions.get('window').width,
+};
