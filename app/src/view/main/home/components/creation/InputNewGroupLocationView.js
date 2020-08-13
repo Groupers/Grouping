3632726922df0a18068788f97d84328d6f80e043
jinsumin <suminjin@naver.com>
2020-08-13 18:50:26 +0900

    new group creation 3-1/4, 3-2/4 location, description

--- app/src/view/main/home/components/creation/InputNewGroupLocationView.js
+++ app/src/view/main/home/components/creation/InputNewGroupLocationView.js
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
 import { inject, observer } from 'mobx-react';
+import Colors from 'react-native/Libraries/NewAppScreen/components/Colors';
 import { GROUPING_CREATION_VIEW_STATUS } from '../../../../../constant/GroupingCreationViewStatus';
 import AddressSearchResultView from '../../../AddressSearchResultView';
 import AddressSearchTextViewTEST from '../../../AddressSearchTextView';
+import { WINDOW_SIZE } from '../../../../../constant/WindowSize';
+import AddressSearchTextView from "../../../AddressSearchTextView";
 
 // eslint-disable-next-line react/prop-types
 const InputNewGroupLocationView = (props) => {
@@ -11,17 +14,47 @@ const InputNewGroupLocationView = (props) => {
     await props.groupingCreationMainStore.groupingAddressSearchKeywordChanged(keyword);
   };
 
+  const rightIconStyle = (groupingCreationView) => {
+    return {
+      marginRight: 15 * WINDOW_SIZE.WIDTH_WEIGHT,
+      fontSize: 18 * WINDOW_SIZE.WIDTH_WEIGHT,
+      color: props.groupingCreationMainStore.isHeaderRightIconActivated(groupingCreationView)
+        ? Colors.white
+        : '#999',
+    };
+  };
+
+  const onHeaderNextButtonClicked = () => {
+    props.groupingCreationMainStore.groupingCreationViewChanged(
+      GROUPING_CREATION_VIEW_STATUS.LOCATION
+    );
+    // props.navigation.navigate('InputNewGroupMoreInfo');
+  };
+
   const onAddressSelected = (address) => {
     props.groupingCreationMainStore.groupingAddressSelected(address);
     props.groupingCreationMainStore.groupingCreationViewChanged(
       GROUPING_CREATION_VIEW_STATUS.EXTRA_INFO
     );
+
+    props.navigation.setOptions({
+      headerRight: () => (
+        <Text
+          onPress={() => {
+            onHeaderNextButtonClicked();
+          }}
+          style={rightIconStyle(GROUPING_CREATION_VIEW_STATUS.MAIN_INFO)}
+        >
+          완료
+        </Text>
+      ),
+    });
   };
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>input group location</Text>
-      <AddressSearchTextViewTEST
+      <AddressSearchTextView
         onChangeText={onAddressKeywordChanged.bind(this)}
         value={props.groupingCreationMainStore.groupingAddressSearchKeyword}
       />
