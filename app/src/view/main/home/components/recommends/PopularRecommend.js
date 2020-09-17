5af6ace7f5996b13ced17caaca8e15c086257a00
rlaal <rlaalgus132@naver.com>
2020-09-17 15:24:50 +0900

    homeMain

--- app/src/view/main/home/components/recommends/PopularRecommend.js
+++ app/src/view/main/home/components/recommends/PopularRecommend.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import { WINDOW_SIZE } from '../../../../../constant/WindowSize';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Icon } from 'react-native-elements';
+import { WINDOW_SIZE } from '../../../../../constant/WindowSize';
+import { COLORS } from '../../../../../assets/Colors';
 
 const PopularRecommend = () => {
   return (
     <View>
       <View style={styles.textBlock}>
-        <TouchableOpacity>
-          <Text style={styles.textStyle}>인기 키워드 ></Text>
+        <TouchableOpacity style={{ flexDirection: 'row' }}>
+          <Text style={styles.textStyle}>지금 내 주변 인기 그룹</Text>
+          <Icon name="keyboard_arrow_right" />
         </TouchableOpacity>
       </View>
       <View style={styles.carouselBox}>
@@ -16,10 +19,21 @@ const PopularRecommend = () => {
           <View style={styles.sampleStyle} />
         </View>
         <View>
-          <Text>같이 다이어트해요</Text>
-          <Text>
-            6 <Text>삼청동</Text>
+          <Text
+            style={{
+              marginTop: 10 * WINDOW_SIZE.HEIGHT_WEIGHT,
+              fontSize: 14 * WINDOW_SIZE.HEIGHT_WEIGHT,
+              fontWeight: 'bold',
+            }}
+          >
+            인사동 냥집사 모여라
           </Text>
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <Icon name="person" size={14} color={COLORS.FONT_GRAY} />
+            <Text style={{ fontSize: 12 * WINDOW_SIZE.HEIGHT_WEIGHT, color: COLORS.FONT_GRAY }}>
+              삼청동
+            </Text>
+          </View>
         </View>
       </View>
     </View>
@@ -28,10 +42,10 @@ const PopularRecommend = () => {
 
 const styles = StyleSheet.create({
   textBlock: {},
-  textStyle: { fontSize: 20 * WINDOW_SIZE.WIDTH_WEIGHT, fontWeight: 'bold' },
+  textStyle: { fontSize: 20 * WINDOW_SIZE.HEIGHT_WEIGHT, fontWeight: 'bold', color: COLORS.BLACK },
   sampleStyle: {
-    height: 70 * WINDOW_SIZE.WIDTH_WEIGHT,
-    width: 70 * WINDOW_SIZE.WIDTH_WEIGHT,
+    height: 144 * WINDOW_SIZE.HEIGHT_WEIGHT,
+    width: 144 * WINDOW_SIZE.HEIGHT_WEIGHT,
     borderRadius: 35 * (WINDOW_SIZE.HEIGHT_WEIGHT / WINDOW_SIZE.WIDTH_WEIGHT),
     backgroundColor: 'purple',
   },
