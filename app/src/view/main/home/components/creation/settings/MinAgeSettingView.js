a56114166009222aa3c35c9cd571636dbbbe9e98
jinsumin <suminjin@naver.com>
2020-10-29 12:12:29 +0900

    set age setting modal view in group creation

--- app/src/view/main/home/components/creation/settings/MinAgeSettingView.js
+++ app/src/view/main/home/components/creation/settings/MinAgeSettingView.js
@@ -1,15 +1,12 @@
 import * as React from 'react';
 import { View, StyleSheet, Text, ScrollView, TouchableOpacity, TextInput } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
-// import { WheelPicker } from 'react-native-wheel-picker-android/index';
 import { WINDOW_SIZE } from '../../../../../../constant/WindowSize';
 import { COLORS } from '../../../../../../assets/Colors';
 
 const MinAgeSettingView = (props) => {
-  // const wheelPickerData = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday'];
-
   const ages = Array.from({ length: 80 }, (v, i) => i);
-  ages.unshift('', '제한없음');
+  ages.unshift('제한없음');
 
   const change = ({ nativeEvent }) => {
     props.onChangeText(Math.ceil(nativeEvent.contentOffset.y / 41) - 2);
@@ -22,11 +19,10 @@ const MinAgeSettingView = (props) => {
         flexDirection: 'row',
         marginTop: 36 * WINDOW_SIZE.HEIGHT_WEIGHT,
         alignItems: 'center',
-        width: 140 * WINDOW_SIZE.WIDTH_WEIGHT,
+        width: 120 * WINDOW_SIZE.WIDTH_WEIGHT,
       }}
     >
       <Text style={{ fontSize: 20 * WINDOW_SIZE.HEIGHT_WEIGHT }}>최소</Text>
-      {/* <WheelPicker indicatorWidth={0.8} data={wheelPickerData} /> */}
       <ScrollView
         scrollToOverflowEnabled
         pagingEnabled
@@ -65,14 +61,14 @@ const MinAgeSettingView = (props) => {
           </TouchableOpacity>
         ))}
       </ScrollView>
-      <TextInput
-        maxLength={50}
-        style={styles.age}
-        autoCorrect={false}
-        placeholder="최소 나이 입력"
-        value={props.groupingMinAge}
-        onChangeText={props.onChangeText != null ? (minAge) => props.onChangeText(minAge) : null}
-      />
+      {/* <TextInput */}
+      {/*  maxLength={50} */}
+      {/*  style={styles.age} */}
+      {/*  autoCorrect={false} */}
+      {/*  placeholder="최소 나이 입력" */}
+      {/*  value={props.groupingMinAge} */}
+      {/*  onChangeText={props.onChangeText != null ? (minAge) => props.onChangeText(minAge) : null} */}
+      {/* /> */}
     </View>
   );
 };
@@ -91,7 +87,7 @@ const styles = StyleSheet.create({
   scroll: {
     opacity: 50,
     height: 52 * WINDOW_SIZE.HEIGHT_WEIGHT,
-    width: 100 * WINDOW_SIZE.WIDTH_WEIGHT,
+    width: 120 * WINDOW_SIZE.WIDTH_WEIGHT,
     borderTopWidth: 1,
     borderBottomWidth: 1,
     borderColor: COLORS.SUB_COLOR,
