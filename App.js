a016ce49a7a34cc019e793cbea8f23647a1a883b
jinsumin <suminjin@naver.com>
2021-02-04 23:59:56 +0900

    get accessToken from AuthTable

--- App.js
+++ App.js
@@ -1,37 +1,46 @@
 import { StyleSheet, View } from 'react-native';
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import { inject, observer } from 'mobx-react';
-import UserTable from './app/src/table/UserTable';
+import AuthTable from './app/src/table/AuthTable';
 import Splash from './app/src/view/Splash';
 import { USER_STATUS } from './app/src/constant/UserStatus';
 import Main from './app/src/view/main/Main';
 import Entrance from './app/src/view/entrance/Entrance';
 
-@inject('userStore', 'friendListStore')
-@observer
-class App extends Component {
-  async componentDidMount() {
+const App = () => {
+  let accessToken = false;
+
+  useEffect(() => {
+    const getAccessToken = async () => {
+      try {
+        accessToken = await AuthTable.objects('Auth');
+      } catch (e) {
+        console.log('no access token');
+      }
+    };
+    getAccessToken().then();
+  }, []);
+
+  /* async componentDidMount() {
     // eslint-disable-next-line react/prop-types,react/destructuring-assignment
     // await this.props.userStore.ready();
     // await this.props.friendListStore.ready();
-  }
+  } */
 
-  render() {
-    let view;
-    if (this.props.userStore.userStatus === USER_STATUS.READY) {
-      view = <Splash />;
-    } else if (this.props.userStore.userStatus === USER_STATUS.GUEST) {
+  let view;
+  if (this.props.userStore.userStatus === USER_STATUS.READY) {
+    view = <Splash />;
+  } else if (this.props.userStore.userStatus === USER_STATUS.GUEST) {
+    view = <Entrance />;
+  } else if (this.props.userStore.userStatus === USER_STATUS.USER) {
+    if (accessToken) {
+      view = <Main />;
+    } else {
       view = <Entrance />;
-    } else if (this.props.userStore.userStatus === USER_STATUS.USER) {
-      if (UserTable.schema.properties.accessToken) {
-        view = <Main />;
-      } else {
-        view = <Entrance />;
-      }
     }
-    return <View style={styles.body}>{view}</View>;
   }
-}
+  return <View style={styles.body}>{view}</View>;
+};
 
 const styles = StyleSheet.create({
   body: {
@@ -39,4 +48,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
+export default inject('userStore', 'friendListStore')(observer(App));
